fe4e81a01605a9728105651ce35336508280a0d4
Prashant <prashantsom75@gmail.com>
2022-12-07 19:24:16 +0530

    code fixed for prime

--- prime.js
+++ prime.js
@@ -1,11 +1,25 @@
 funnction prime(num){
   
+let counter = 0
   for(let i=1;i<=num;i++){
      if(num%i==0){
-      count++
+      counter++
   }
+}
+  if (counter==2){
+     return true;
+ }
+else return false;
+ 
 }
 
 for(let i=0;i<=20;i++){
-  prime(i)
+  let answer = prime(i);
+  
+  if(ans == true){
+    console.log(i,"is a prime number")
+ }
+  else {
+      console.log(i,"is not a prime number" 
+ }
 }
